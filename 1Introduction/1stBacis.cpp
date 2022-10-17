d38fd4f9c0a8de289cf8e296214a9a6435dae8b5
Aiman <aimananjum2001@gmail.com>
2022-10-18 00:50:31 +0530

    updated

--- 1Introduction/1stBacis.cpp
+++ 1Introduction/1stBacis.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
+    //SI
     int principleAmount,interest,timePeriod;
     cout<<"Enter principle, interest and time period"<<endl;
     cin>>principleAmount;
@@ -10,5 +11,13 @@ int main(){
     float SI = (principleAmount * timePeriod * interest)/100;
     cout<<"Simple Interest is: "<<SI;
     
+    //a<b?
+    int a,b;
+    cin>>a;
+    cin>>b;
+    if(a<b)
+    cout<<"Yes";
+    else
+    cout<<"No";
     return 0;
 }
