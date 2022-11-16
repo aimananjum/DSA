4c24d42e21898eb68ad09c0989ca0dc136567bbd
Aiman Jamadar <84615508+aimananjum@users.noreply.github.com>
2022-11-16 17:03:49 +0530

    Create DeleteNode.cpp

---
+++ tree/DeleteNode.cpp
@@ -0,0 +1,19 @@
+//Delete a node from a singly linked list without having head pointer
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* nxtNode = node->next;
+        node->val=nxtNode->val;
+        node->next=nxtNode->next;
+        delete(nxtNode);
+    }
+};
