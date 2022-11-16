e390862a97c5b8e9fe6d4fffdbef5e4b54738c3e
Aiman Jamadar <84615508+aimananjum@users.noreply.github.com>
2022-11-16 15:36:15 +0530

    Create initializingArray.cpp

---
+++ basics/initializingArray.cpp
@@ -0,0 +1,18 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n = 10;
+    int val = 5;
+ 
+    int arr[n];
+    fill_n (arr, 10, -3);
+ 
+    for(int i=0;i<n;i++){
+      cout<<arr[i]<<" ";
+    }
+ 
+    return 0;
+}
