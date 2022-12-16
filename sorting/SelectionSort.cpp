75c92ba6a045eb861a9c606478bfb273efb3aa4c
Aiman <aimananjum2001@gmail.com>
2022-12-16 17:02:05 +0530

    All Folders

--- sorting/SelectionSort.cpp
+++ sorting/SelectionSort.cpp
@@ -3,14 +3,26 @@
 #include <vector>
 using namespace std;
 void selectionSort(vector<int> & a){
-    return ;
+    int n = a.size();
+    for(int i=0;i<n;i++){
+        int minIdx = i;
+        for(int j =i+1;j<n;i++){
+            if(a[minIdx]<=a[j]){
+                minIdx = j;
+            }
+        }
+        swap(a[minIdx],a[i]);
+    }
+    return;
 }
 int main(){
     int n;
     cin>>n;
     vector<int> a;
     for(int i=0;i<n;i++){
-        cin>>a[i];
+        int b;
+        cin>>b;
+        a.push_back(b);
     }
     selectionSort(a);
     for(int i=0;i<n;i++){
