ec9b56449592470f17fe689e75f44365835f9303
Aiman <aimananjum2001@gmail.com>
2023-04-30 21:04:02 +0530

    graph basics

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # DSA
+
+command to run in terminal:
+
+1-Go to the deried path
+
+2-To compile:
+    g++ -o a.exe filename.cpp
+
+3-To execute:
+    ./a.exe
--- graph/graphBasics.cpp
+++ graph/graphBasics.cpp
@@ -2,7 +2,42 @@
 #include<iostream>
 using namespace std;
 
+class graph{
+    public:
+
+    unordered_map<int,list<int>> adj;
+    
+    void addEdge(int u,int v,bool direction){
+        adj[u].push_back(v);
+        if(direction==0){
+            adj[v].push_back(u);
+        }
+    }
+
+    void printAdjList(){
+        for(auto i:adj){
+            cout<<i.first<<"->";
+            for(auto j:i.second){
+                cout<<j<<",";
+            }
+            cout<<endl;
+        }
+    } 
+};
+
 int main(){
-cout<<"HELLO WORLD";
-return 0;
+    int n,m;
+    cout<<"Enter No. of Nodes";
+    cin>>n;
+    cout<<"Enter No of Edges";
+    cin>>m;
+    graph g;
+	for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        g.addEdge(u,v,0);
+    }
+    g.printAdjList();
+    
+    return 0;
 }
